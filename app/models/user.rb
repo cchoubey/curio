1292815003ac1fd0b97bb3e528a752af62e2e50b
chandanchoubey <chandanchoubey@msn.com>
2021-08-23 16:00:08 +1200

    password confirmation and added first name and last name

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        # validates_confirmation_of :password
+end
