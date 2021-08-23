974708d5dfc073d0e1817c602bd0165555d3a720
chandanchoubey <chandanchoubey@msn.com>
2021-08-24 11:15:35 +1200

    aded default value to active and admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   scope:order_by_first_name, -> { order('LOWER(first_name)') }
+  validates :first_name, presence: true
 end
